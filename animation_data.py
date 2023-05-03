322ff5d7a5b97ea678654eb2ab4976064ae0997a
DimaLeon2000 <dmitryaveron2000@gmail.com>
2023-05-04 07:26:07 +0900

    Moved from TKinter to Pygame, and fixed sequence parsing functions.

--- animation_data.py
+++ animation_data.py
@@ -1,11 +1,14 @@
 from animation_reader import FASReader
+import io
 import re
+from PIL import Image, ImageOps
 from ast import literal_eval
 
-REGEX_EXTRA_SPRITE = r'(#{1,2})(.+[^,])' # places a temporary extra sprite
+REGEX_EXTRA_SPRITE = r'(#{1,2})(.+[^,])'  # places a temporary extra sprite
 # (group #1: absolute/relative positioning; group #2: sequence)
 REGEX_FENCING = r'©(?:\[(-?[0-9]+)\,(-?[0-9]+)\])?(\d[lcr]\d[tmb])\,(?:\[(-?[0-9]+)\,(-?[0-9]+)\])?(\d[lcr]\d[tmb])©'
 REGEX_FRAME_RANGE = r'([0-9]+)-([0-9]+)'
+REGEX_GROUP = r'\((.+?)\)'
 REGEX_OFFSET = r'\[(-?[0-9]+,-?[0-9]+)\]'
 REGEX_RANDOM_CHOICES = r'{(.+?)}'
 REGEX_RANDOM_CHOICES_CHOICE_WEIGHTED = r'(\(.+?\)|[a-zA-Z_0-9]+|[0-9]+)%([0-9]+)'
@@ -32,32 +35,49 @@ class FASData:
         for i in range(self.reader.seq_header['seq_count']):
             sequence = self.reader.read_sequence(i)
             self.sequences[sequence['name']] = sequence['sequence']
-            # print(sequence['name'] + ':', self.sequences[sequence['name']])
-            # print(sequence['name']+' (parsed): ', self.parse_sequence(seq=sequence['name']))
+            print(sequence['name'] + ':', self.sequences[sequence['name']])
+            print('== ' + sequence['name'] + ' (parsed) ==')
+            self.parse_sequence(seq=sequence['name'])
         self.frames_header = self.reader.frames_header
         self.frames_bitmap = self.reader.frames_bitmap
 
-    def parse_sequence_part(self, __part):
-        # print(__part.isnumeric())
-        return __part
+    def parse_sequence_part(self, __part):  # INCOMPLETE
+        if re.match(REGEX_GROUP, __part):
+            return re.match(REGEX_GROUP, __part).group(0)
+        elif re.match(REGEX_FENCING, __part):
+            # print(__part, '(FENCING)')
+            return 'FENCING'
+        elif re.match(REGEX_OFFSET, __part):
+            print(literal_eval(re.match(REGEX_OFFSET, __part).groups()[0]), 'OFFSET')
+            return literal_eval(re.match(REGEX_OFFSET, __part).groups()[0])
+        else:
+            return __part
 
     def parse_sequence(self, seq):  # INCOMPLETE
-        parts = re.split(r',(?![^()]*\))(?![^\[\]]*])(?![^{}]]*})(?![^\xA9]*\xA9)', self.sequences[seq])
+        parts = re.findall(r'(?:\((?:[^()].*?)*\)|\[(?:[^[\]].*?)*]|{(?:[^{}].*?)*}|©(?:[^©].*?)*©|[^,])+',
+                           self.sequences[seq])
         for x in parts:
-            self.parse_sequence_part(x)
+            x = self.parse_sequence_part(x)
+            print(x)
             # if re.match(r'\[([0-9\,\-]+)\]', x): #check if
             #     pos_offset = literal_eval(re.match(r'\[([0-9\,\-]+)\]', x).group(1))
             #     print(pos_offset)
             # print(x)
-            pass
+            # pass
         return parts
 
-    def get_frame_bitmap(self, __frame, mask=bool):
+    def get_frame_bitmap(self, __frame, mask):
         bitmap_info = self.bitmap_infos[self.frames_header['info'][__frame] * 2 + int(mask)]
         bitmap = self.frames_bitmap[__frame]
-        header = b'BM' + int(len(bitmap_info) + len(bitmap) + 14).to_bytes(length=4,byteorder='little', signed=False) \
-        + (0).to_bytes(length=2, byteorder='little', signed=False) \
-        + (0).to_bytes(length=2, byteorder='little', signed=False) \
-        + int(len(bitmap_info) + 14).to_bytes(length=4, byteorder='little', signed=False)
+        header = b'BM' + int(len(bitmap_info) + len(bitmap) + 14).to_bytes(length=4, byteorder='little', signed=False) \
+            + (0).to_bytes(length=2, byteorder='little', signed=False) \
+            + (0).to_bytes(length=2, byteorder='little', signed=False) \
+            + int(len(bitmap_info) + 14).to_bytes(length=4, byteorder='little', signed=False)
         data = header + bitmap_info + bitmap
         return data
+
+    def get_frame_masked(self, __frame):
+        color = Image.open(io.BytesIO(self.get_frame_bitmap(__frame, mask=False))).convert('RGBA')
+        mask = ImageOps.invert(Image.open(io.BytesIO(self.get_frame_bitmap(__frame, mask=True))).convert('L'))
+        color.putalpha(mask)
+        return color
--- main.py
+++ main.py
@@ -1,20 +1,26 @@
 from audio_data import WASData
 from animation_data import FASData
-from bit_reader import *
-import io
-from PIL import Image, ImageTk
+# from bit_reader import *
+# import io
+# import PIL
 import pyaudio
+import pygame as pg
+from settings import *
 import struct
-import tkinter
+import sys
+# import tkinter
 import wave
 import os
 import zlib
 
 
-WIN_SIZE = WIDTH, HEIGHT = 320, 240
 LINE_UP = '\033[1A'
 LINE_CLEAR = '\x1b[2K'
 
+
+def pilImageToSurface(pilImage):
+    return pg.image.frombytes(
+        pilImage.tobytes(), pilImage.size, pilImage.mode).convert_alpha()
 
 def debug_was_tool(audio_path):  # for reading .WAS files, that contain 16-bit PCM audio data
     chunk_length = 32  # for buffering
@@ -105,33 +111,86 @@ def faz_inflate(packed_path):
         anim_file.close()
 
 
-def draw_frame(__frame_num):
-    if test_fas.frames_header['count'] >= 1:
-        # print(__frame_num)
-        image_data = test_fas.get_frame_bitmap(__frame_num, mask=False)
-        image_frame = ImageTk.PhotoImage(image_data)
-        frame_canvas.create_image(0, 0, image=image_frame, anchor=tkinter.NW)
-        root.title('Frame #' + f"{test_fas.frames_header['id'][__frame_num]:04d}")
+class SpriteUnit(pg.sprite.Sprite):
+    def __init__(self, handler, x, y):
+        self.handler = handler
+        self.x, self.y = x, y
+        super().__init__(handler.group)
+        self.image_ind = frame_num
+        self.image = self.handler.images[frame_num]
+        self.rect = self.image.get_rect()
+
+    def update(self):
+        self.rect.center = self.x, self.y
+        self.image = self.handler.images[self.image_ind]
 
 
-def terminate(event):
-    # print(event)
-    global frame_num
-    if event.keycode == 39:
-        if frame_num < test_fas.frames_header['count']-1:
-            frame_num += 1
-            draw_frame(frame_num)
-    elif event.keycode == 37:
-        if frame_num > 0:
-            frame_num -= 1
-            draw_frame(frame_num)
-    if event.keycode == 81:
-        root.destroy()
+class SpriteHandler:
+    def __init__(self, app):
+        self.app = app
+        self.images = [pilImageToSurface(test_fas.get_frame_masked(i)) for i in range(test_fas.frames_header['count'])]
+        # self.mask = pg.image.load(io.BytesIO(get_frame_bitmap(frame_num, True)))
+        self.group = pg.sprite.Group()
+        self.sprites = [SpriteUnit(self, WIDTH // 2, HEIGHT // 2)]
+
+    def update(self):
+        self.group.update()
+
+    def draw(self):
+        self.group.draw(self.app.screen)
 
 
-def mouse_click(event):
-    print(event)
-    print(touch_map.get(event.x, event.y))
+class App:
+    def __init__(self):
+        pg.init()
+        self.screen = pg.display.set_mode(WIN_SIZE)
+        self.clock = pg.time.Clock()
+        self.font = pg.freetype.SysFont('Courier New', FONT_SIZE)
+        self.dt = 0.0
+        self.sprite_handler = SpriteHandler(self)
+        self.sprite_handler.sprites[0].sequence = 'ab'
+
+    def update(self):
+        pg.display.flip()
+        self.sprite_handler.update()
+        # self.dt = self.clock.tick() * 0.001
+        self.dt = self.clock.tick(10)
+
+    def draw(self):
+        self.screen.fill('gray64')
+        self.sprite_handler.draw()
+        self.draw_fps()
+
+    def draw_fps(self):
+        fps_text = f'{self.clock.get_fps() :.0f} FPS'
+        seq_text = f'Current sequence: {self.sprite_handler.sprites[0].sequence}'
+        frame_text = f'Current frame: {test_fas.frames_header["id"][self.sprite_handler.sprites[0].image_ind]:04d}'
+        self.font.render_to(self.screen, (8, 8), text=fps_text, fgcolor='black')
+        self.font.render_to(self.screen, (8, 8+FONT_SIZE), text=seq_text, fgcolor='black')
+        self.font.render_to(self.screen, (8, 8+FONT_SIZE*2), text=frame_text, fgcolor='black')
+
+    def check_events(self):
+        for e in pg.event.get():
+            if e.type == pg.KEYDOWN:
+                if e.key == pg.K_RIGHT:
+                    if self.sprite_handler.sprites[0].image_ind < test_fas.frames_header['count'] - 1:
+                        self.sprite_handler.sprites[0].image_ind += 1
+                elif e.key == pg.K_LEFT:
+                    if self.sprite_handler.sprites[0].image_ind > 0:
+                        self.sprite_handler.sprites[0].image_ind -= 1
+                elif e.key == pg.K_HOME:
+                    self.sprite_handler.sprites[0].image_ind = 0
+                elif e.key == pg.K_END:
+                    self.sprite_handler.sprites[0].image_ind = test_fas.frames_header['count'] - 1
+            if e.type == pg.QUIT or (e.type == pg.KEYDOWN and e.key == pg.K_ESCAPE):
+                pg.quit()
+                sys.exit()
+
+    def run(self):
+        while True:
+            self.check_events()
+            self.update()
+            self.draw()
 
 
 if __name__ == '__main__':
@@ -139,51 +198,37 @@ if __name__ == '__main__':
     # left=min(pnt1[0],(pnt2.[0]-frame_width))
     # top=min(-pnt1[1],(-pnt2.[1]-frame_width))
     test_fas = FASData(r'TEST_FILE.FAS')
-    root = tkinter.Tk()
-    if hasattr(test_fas, 'touch'):  # get hotspots
-        touch_map = tkinter.PhotoImage(width=test_fas.header['width'], height=test_fas.header['height'])
-        colors = len(test_fas.touch['colors'])
-        if colors in range(1, 3):
-            bpp = 1
-        elif colors <= 4:
-            bpp = 2
-        elif colors <= 16:
-            bpp = 4
-        elif colors <= 256:
-            bpp = 8
-        else:
-            bpp = 0
-        # put pixels into hotspots map
-        for i in range(test_fas.header['height']):
-            for j in range(test_fas.header['width']):
-                palette_num = read_bits_per_pixel(test_fas.touch['bitmap'][j // (8 // bpp)
-                                                  + i * test_fas.touch['width']], bpp)[j % (8 // bpp)]
-                touch_map.put('#%02x%02x%02x' % read_rgb(test_fas.touch['colors'][palette_num], False),
-                              (j, test_fas.header['height'] - i - 1))
-        touch_canvas = tkinter.Canvas(root,
-                                      width=test_fas.header['width'],
-                                      height=test_fas.header['height'],
-                                      highlightthickness=0)
-        touch_canvas.pack()
-        touch_canvas.create_image(0, 0, image=touch_map, anchor=tkinter.NW)
-
-    frame_canvas = tkinter.Canvas(root,
-                                  width=test_fas.header['width'],
-                                  height=test_fas.header['height'],
-                                  highlightthickness=0)
-    frame_canvas.pack()
-    # image_frame = ImageTk.PhotoImage(width=test_fas.header['width'], height=test_fas.header['height'])
     # frame_num = test_fas.frames_header['count'] - 1
-    # frame_id = 4000; frame_num = test_fas.frames_header['id'].index(frame_id)
-    if test_fas.frames_header['count'] >= 1:
-        frame_num = 0
-        root.title('Rendering...')
-        # draw_frame(frame_num)
-        image_frame = ImageTk.PhotoImage(Image.open(io.BytesIO(test_fas.get_frame_bitmap(frame_num, mask=False))))
-        frame_canvas.create_image(0, 0, image=image_frame, anchor=tkinter.NW)
-        root.title('Frame #'+f"{test_fas.frames_header['id'][frame_num]:04d}")
-        root.bind_all('<Key>', terminate)
-        root.bind_all('<Button>', mouse_click)
-        root.after(1000,draw_frame,frame_num+1)
-        root.configure(bg='black')
-        root.mainloop()
+    # frame_id = 4000;
+    # frame_num = test_fas.frames_header['id'].index(frame_id)
+    frame_num = 0
+    app = App()
+    app.run()
+    # root = tkinter.Tk()
+
+    # if hasattr(test_fas, 'touch'):  # get hotspots
+    #     touch_map = tkinter.PhotoImage(width=test_fas.header['width'], height=test_fas.header['height'])
+    #     colors = len(test_fas.touch['colors'])
+    #     if colors in range(1, 3):
+    #         bpp = 1
+    #     elif colors <= 4:
+    #         bpp = 2
+    #     elif colors <= 16:
+    #         bpp = 4
+    #     elif colors <= 256:
+    #         bpp = 8
+    #     else:
+    #         bpp = 0
+    #     # put pixels into hotspots map
+    #     for i in range(test_fas.header['height']):
+    #         for j in range(test_fas.header['width']):
+    #             palette_num = read_bits_per_pixel(test_fas.touch['bitmap'][j // (8 // bpp)
+    #                                               + i * test_fas.touch['width']], bpp)[j % (8 // bpp)]
+    #             touch_map.put('#%02x%02x%02x' % read_rgb(test_fas.touch['colors'][palette_num], False),
+    #                           (j, test_fas.header['height'] - i - 1))
+    #     touch_canvas = tkinter.Canvas(root,
+    #                                   width=test_fas.header['width'],
+    #                                   height=test_fas.header['height'],
+    #                                   highlightthickness=0)
+    #     touch_canvas.pack()
+    #     touch_canvas.create_image(0, 0, image=touch_map, anchor=tkinter.NW)
